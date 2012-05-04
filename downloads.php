ba6a54afc9f101e8171514d6057e1e7f706361ab
Alexander Dean <dstar@csh.rit.edu>
2012-05-04 16:39:34 -0400

    Added all the blank files we'll need.

---
+++ downloads.php
@@ -0,0 +1,9 @@
+<?php
+
+	require_once 'include/scurvy/src/scurvy.php';
+
+	$template = new Scurvy( 'downloads.html', 'templates/' );
+
+	$template->set( 'title', 'Extensible Monitoring Platform' );
+
+	echo $template->render();
